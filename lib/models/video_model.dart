72926232912b7f8da3cde713072aff41b3afa8cb
Joho456 <josewambua008@gmail.com>
2026-02-11 20:02:44 +0300

    Create VideoModel class

---
+++ lib/models/video_model.dart
@@ -0,0 +1,11 @@
+class VideoModel {
+  final String id;
+  final String title;
+  final String url;
+  final String description;
+  final DateTime createdAt;
+
+  VideoModel({required this.id, required this.title, required this.url, required this.description, required this.createdAt});
+
+  // Add any methods or utilities you might need here
+}
