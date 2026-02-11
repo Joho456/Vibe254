1aced1195823928dcbaf81d66b602cbecd8f5ddf
Joho456 <josewambua008@gmail.com>
2026-02-11 20:04:14 +0300

    Create custom button widget

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color color;
+
+  CustomButton({required this.text, required this.onPressed, this.color = Colors.blue});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: color,
+      ),
+      child: Text(text),
+    );
+  }
+}
